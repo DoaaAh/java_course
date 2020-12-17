d8b0ea8d6f0757c8a66eea13a27615d929f69058
DoaaAh <doaa_ahmedali@outlook.com>
2020-12-17 16:21:25 +0200

    String processor update

--- src/chapter8/TextProcessor.java
+++ src/chapter8/TextProcessor.java
@@ -3,6 +3,8 @@ package chapter8;
 public class TextProcessor {
     public static void main(String[] arg){
         countWords("I love BTS");
+        reverseString("BTS is the best");
+        addSpaces("JinYoongiHopeNamjoonJiminTaeKook");
     }
     public static void countWords(String text){
         var words = text.split(" ");
@@ -14,4 +16,22 @@ public class TextProcessor {
             System.out.println(words[i]);
         }
     }
+    public static void reverseString(String text){
+        for(int i = text.length()-1; i>=0; i--){
+            System.out.print(text.charAt(i));
+        }
+        System.out.print("\n");
+
+    }
+    public static void addSpaces(String text){
+        var modifiedText = new StringBuilder(text);
+        for (int i = 0; i<modifiedText.length(); i++){
+            if (i!=0 && Character.isUpperCase(modifiedText.charAt(i))){
+                modifiedText.insert(i, " ");
+                i++; // to skip the added space
+            }
+        }
+        System.out.print(modifiedText);
+    }
 }
+
