3b045400a21dbaba497f12f3a3cee33479dce631
DoaaAh <doaa_ahmedali@outlook.com>
2020-12-17 15:48:34 +0200

    chapter 8

---
+++ src/chapter8/TextProcessor.java
@@ -0,0 +1,17 @@
+package chapter8;
+
+public class TextProcessor {
+    public static void main(String[] arg){
+        countWords("I love BTS");
+    }
+    public static void countWords(String text){
+        var words = text.split(" ");
+        int numberOfWords = words.length;
+        String message = String.format("Your text contains %d words", numberOfWords);
+        System.out.println(message);
+
+        for(int i = 0;i<numberOfWords; i++){
+            System.out.println(words[i]);
+        }
+    }
+}
