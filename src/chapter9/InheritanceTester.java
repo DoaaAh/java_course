3dc24167ea8ea7e5c61cad56af89124335c9e640
DoaaAh <doaa_ahmedali@outlook.com>
2020-12-22 11:48:26 +0200

    Inheritance final updates

--- src/chapter9/InheritanceTester.java
+++ src/chapter9/InheritanceTester.java
@@ -1,7 +1,13 @@
 package chapter9;
 
 public class InheritanceTester {
-    public static void main(String[] arg){
+    public static void main(String[] arg) {
+        Mother mother = new Mother();
+        mother.setName("Nadia");
+        System.out.println(mother.getName() + " is a " + mother.getGender());
+    }
+
+    public static void testOverload() {
         Rectangle rectangle = new Rectangle();
         rectangle.print();
 
